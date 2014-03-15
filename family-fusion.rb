6e2556c500dd8407fc9709e41cbe0c22eb9dcbb4
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-03-15 16:09:41 -0600

    Add a bunch of Twilio boilerplate. Does it work?

--- family-fusion.rb
+++ family-fusion.rb
@@ -18,8 +18,11 @@ set :haml, :format => :html5, :layout => true
 Dotenv.load
 
 # set up twilio
-account_sid = ENV['TWILIO_SID']
-auth_token = ENV['TWILIO_TOKEN']
+ACCOUNT_SID = ENV['TWILIO_SID']
+AUTH_TOKEN = ENV['TWILIO_TOKEN']
+CALLER_ID = '+17139994373' # why yes I am hardcoding this
+BASE_URL = 'http://familyfusion.herokuapp.com/'
+@client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
 # set up basic caching
 #set :static_cache_control, [:public, :max_age => 300]
@@ -28,8 +31,47 @@ auth_token = ENV['TWILIO_TOKEN']
 #end
 
 get '/' do
+  # @call = @client.account.calls.create(
+  #   from: '+17139994373',
+  #   to: "+16173790642",
+  #   url: 
+  # )
   haml :main, locals: { title: "Hello, world!" }
 end
+
+post '/makecall' do
+   # Use the Twilio REST API to initiate an outgoing call
+  if !params['number']
+    redirect_to '/error', 'msg' => 'Invalid phone number'
+    return
+  end
+ 
+  # parameters sent to Twilio REST API
+  data = {
+    :from => CALLER_ID,
+    :to => params['number'],
+    :url => BASE_URL + '/reminder',
+  }
+
+  begin
+    client = Twilio::REST::Client.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+    client.account.calls.create data
+  rescue StandardError => bang
+    redirect_to '/error', 'msg' => "Error #{bang}"
+    return
+  end
+ 
+  redirect_to '/calling', 'msg' => "Calling #{params['number']}..."
+end
+
+get '/calling' do
+  "Outgoing call! :: #{msg}, #{request.inspect}"
+end
+
+get '/error' do
+  "Something went wrong: #{msg}, #{request.inspect}"
+end
+
 
 # get '/images/:image' do |image|
 #   redirect 
