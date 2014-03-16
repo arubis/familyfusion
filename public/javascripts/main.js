f41e5c0bfee6dfdbddcff9bcb0db663612d2eaf4
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-03-15 23:16:36 -0600

    Some happy animations WHY AM I STILL HERE

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var singlePad = false; // track whether we've expanded to a single pad
 
   function getPadHeight() { return Math.max(($(window).height() - 200) / 2, 30); }
 
@@ -6,17 +7,26 @@ $(document).ready(function() {
   $('.pad').css('line-height', getPadHeight() + "px")
 
   $(window).resize(function() {
-    $('.pad').height(getPadHeight());
-    $('.pad').css('line-height', getPadHeight() + "px")
+    if(!singlePad) {
+      $('.pad').height(getPadHeight());
+      $('.pad').css('line-height', getPadHeight() + "px")
+    }
   });
 
   // good lord I am exhausted. Just get one of these bloody things to
   // fire and clean it up in the morning, yeah?
   $('.pad').on('click tap', function() {
     var target = $(this);
-    $.post('/call', "number=+16175002301", function(data, textStatus, xhr) {
-      target.html("Calling someone!");
-    });
+
+    singlePad = true;
+    $('.pad').not(this).hide();
+    $(this).prepend("Calling ");
+    $(this).append("...");
+    var newHeight = $(window).height() - 200;
+    $(this).animate({height: newHeight, 'line-height': newHeight, width: '100%'}, 1000)
+    // $.post('/call', "number=+16175002301", function(data, textStatus, xhr) {
+    //   target.html("Calling someone!");
+    // });
   });
 
 });
