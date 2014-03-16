c2c18951c8fb7166632497d39bd1b7958f1f41ac
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-03-16 10:06:28 -0600

    When the going gets tough, the tough eventually remember semicolons

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
   var singlePad = false; // track whether we've expanded to a single pad
 
+  // We want to size the four dialing pad surfaces continually with
+  // window size so they're always visible and always large
+  // We'll trigger this on load and on viewport resize.
+  // n.b. the line-height readjustments vertically center text 
+  // within each pad surface.
   function getPadHeight() { return Math.max(($(window).height() - 200) / 2, 30); }
 
   $('.pad').height(getPadHeight);
-  $('.pad').css('line-height', getPadHeight() + "px")
+  $('.pad').css('line-height', getPadHeight() + "px");
 
   $(window).resize(function() {
     if(!singlePad) {
       $('.pad').height(getPadHeight());
-      $('.pad').css('line-height', getPadHeight() + "px")
+      $('.pad').css('line-height', getPadHeight() + "px");
     }
   });
 
-  // good lord I am exhausted. Just get one of these bloody things to
-  // fire and clean it up in the morning, yeah?
+  // DIVs aren't actually buttons, but I want them to be.
+  // 
   $('.pad').on('click tap', function() {
     var target = $(this);
 
@@ -24,11 +29,10 @@ $(document).ready(function() {
     var origLocation = $(this).offset();
 
     $('.pad').not(this).fadeOut();
-    $(this).fadeOut().prepend("Calling ").append("...")
+    $(this).fadeOut().prepend("Calling ").append("...");
 
     var newHeight = $(window).height() - 200;
 
-    // $(this).prepend("Calling ").append("...");
     $(this).fadeIn()
            .animate({'top': 0, 'height': newHeight,
                      'line-height': newHeight, 
