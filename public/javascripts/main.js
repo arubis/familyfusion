4fd550aa67718880e46e47b84c8f551a980df551
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-03-15 22:42:00 -0600

    If you click on someone's name a phone call starts and then I go to bed

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
 
-  function getPadHeight() { return ($(window).height() - 200) / 2; }
+  function getPadHeight() { return Math.max(($(window).height() - 200) / 2, 30); }
 
-  $('.grid').height(getPadHeight);
+  $('.pad').height(getPadHeight);
 
   $(window).resize(function() {
-    $('.grid').height(getPadHeight());
+    $('.pad').height(getPadHeight());
+  });
+
+  // good lord I am exhausted. Just get one of these bloody things to
+  // fire and clean it up in the morning, yeah?
+  $('.pad').on('click tap', function() {
+    var target = $(this);
+    $.post('/call', "number=+16175002301", function(data, textStatus, xhr) {
+      target.html("Calling someone!");
+    });
   });
 
 });
